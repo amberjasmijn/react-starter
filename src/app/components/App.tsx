c7d4b9527ca93632ff99cd2813edc205801daca6
Amber Brookhuis <amberbrookhuis@gmail.com>
2018-07-14 21:53:20 +0200

    Init project

---
+++ src/app/components/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+export class App extends React.Component<{}, {}> {
+  constructor(props: {}) {
+    super(props)
+    this.state = { }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hi there!</h1>
+      </div>
+    )
+  }
+}
